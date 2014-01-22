8f6f9bf8f8d3a24e5025c9c7cd6e62ca09ed6ec5
TimBao <dabao0521@gmail.com>
2014-01-22 15:23:55 +0800

    update readme

---
+++ README.mk
@@ -0,0 +1,7 @@
+Othello (White and Black in chess)
+
+From Wikipedia, the free encyclopedia
+
+In chess, the player who moves first is referred to as "White" and the player who moves second is referred to as "Black". Similarly, the pieces that each conducts are called, respectively, "the white pieces" and "the black pieces". The pieces are often not literally white and black, but some other colors (usually a light color and a dark color, respectively). The 64 squares of the chessboard, which is colored in a checkered pattern, are likewise referred to as "white squares" or "light squares" on the one hand, and "black squares" or "dark squares" on the other. In most cases, the squares are not actually white and black, but a light color and a contrasting dark color. For example, the squares on plastic boards are often off-white ("buff") and green, while those on wood boards are often light brown and dark brown.
+
+In old chess writings, the sides are often called Red and Black, because those were the two colors of ink then commonly available when hand-drawing or printing chess position diagrams.
